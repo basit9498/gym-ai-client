d91d426ccb95a9f21f635c8d0e0816efa765fd69
basit9498 <basit9498@gmail.com>
2026-04-22 20:30:01 +0500

    FIX: vercel build

--- src/modules/admin/index.ts
+++ src/modules/admin/index.ts
@@ -2,5 +2,6 @@ export * from './users';
 export * from './analytics';
 export * from './finance';
 export * from './settings';
+export * from './data/admin-mocks';
 export * from './components/AdminTopbar';
 export { default as AdminSidebar } from './components/AdminSidebar';
